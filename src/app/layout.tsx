dc70333adb55036d730bc8ec73966c4729c11233
otabekov-komron <mrOK.blog@gmail.com>
2023-12-14 15:36:12 +0500

    end

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,78 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="../assets/images/logo.webp" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/public/public/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/public/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/public/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/public/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/public/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/public/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/public/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/public/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/public/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/public/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/public/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/public/favicon-16x16.png"
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/public/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body className="font-greycliff text-mainWhite">
         <Navbar />
@@ -123,7 +194,10 @@ export default function RootLayout({
                   </div>
                 </div>
                 <div className="flex gap-5 w-full">
-                  <a target="_blank" href="https://www.facebook.com/hypernova.uz">
+                  <a
+                    target="_blank"
+                    href="https://www.facebook.com/hypernova.uz"
+                  >
                     <svg
                       data-new-gr-c-s-check-loaded
                       data-gr-ext-installed
@@ -146,7 +220,10 @@ export default function RootLayout({
                       </defs>
                     </svg>
                   </a>
-                  <a target="_blank" href="https://www.linkedin.com/company/hypernova-uz/about/?viewAsMember=true">
+                  <a
+                    target="_blank"
+                    href="https://www.linkedin.com/company/hypernova-uz/about/?viewAsMember=true"
+                  >
                     <svg
                       data-new-gr-c-s-check-loaded
                       data-gr-ext-installed
@@ -169,7 +246,10 @@ export default function RootLayout({
                       </defs>
                     </svg>
                   </a>
-                  <a target="_blank" href="https://www.instagram.com/hypernova.uz/">
+                  <a
+                    target="_blank"
+                    href="https://www.instagram.com/hypernova.uz/"
+                  >
                     <svg
                       data-new-gr-c-s-check-loaded
                       data-gr-ext-installed
