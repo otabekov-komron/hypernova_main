c91a5987f428f3e2fcec9773745836acefa09d66
otabekov-komron <mrOK.blog@gmail.com>
2023-12-19 17:48:25 +0500

    the end

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,17 +24,12 @@ export default function RootLayout({
         <link
           rel="apple-touch-icon"
           sizes="57x57"
-          href="/public/public/apple-icon-57x57.png"
+          href="/public/apple-icon-57x57.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="60x60"
           href="/public/apple-icon-60x60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="/apple-icon-72x72.png"
         />
         <link
           rel="apple-touch-icon"
@@ -77,12 +72,6 @@ export default function RootLayout({
           type="image/png"
           sizes="32x32"
           href="/public/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="/public/favicon-96x96.png"
         />
         <link
           rel="icon"
